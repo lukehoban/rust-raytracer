51f8aa112e64a28a8e45e578c7e0ee681761ca76
Luke Hoban <lukehoban@gmail.com>
2015-05-13 06:53:07 -0700

    Intersect struct lifetimes

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ impl Vector {
     fn minus(v1: &Vector, v2: &Vector) -> Vector {
         Vector {x: v1.x-v2.x, y: v1.y-v2.y, z: v1.z-v2.z}
     }
+    fn dot(v1: &Vector, v2: &Vector) -> f64 {
+        v1.x*v2.x+v1.y*v2.y+v1.z*v2.z
+    }
     fn mag(v: &Vector) -> f64 {
         (v.x*v.x + v.y*v.y + v.z*v.z).sqrt()
     }
@@ -33,10 +36,10 @@ impl Color {
 }
 
 struct Ray { dir: Vector, start: Vector }
-struct Intersect { thing: Box<Thing>, ray: Ray, dist: f64}
+struct Intersect<'a> { thing: &'a Thing, ray: Ray, dist: f64}
 trait Thing {
     fn normal(&self, pos: Vector) -> Vector;
-    fn intersect(&self, ray: Ray) -> Intersect;
+    fn intersect<'a>(&'a self, ray: Ray) -> Option<Intersect<'a>>;
 }
 struct Light { pos: Vector, color: Color }
 struct Camera { pos: Vector, lookAt: Vector }
@@ -47,26 +50,39 @@ impl Thing for Sphere {
     fn normal(&self, pos: Vector) -> Vector {
         Vector::norm(&Vector::minus(&pos, &self.center))
     }
-    fn intersect(&self, ray: Ray) -> Intersect {
-        panic!()
+    fn intersect<'a>(&'a self, ray: Ray) -> Option<Intersect<'a>> {
+        let eo = Vector::minus(&self.center, &ray.start);
+        let v = Vector::dot(&eo, &ray.dir);
+        let dist =
+            if v < 0.0 {
+                0.0
+            } else {
+                let disc = self.radius*self.radius - Vector::dot(&eo, &eo) + v*v;
+                if disc < 0.0 { 0.0 } else { v - disc.sqrt() }
+            };
+        if dist == 0.0 {
+            None
+        } else {
+            Some(Intersect { thing: self, ray: ray, dist: dist})
+        }
     }
 }
 
 
-fn intersections(ray: Ray, scene: Scene) -> Option<Intersect>{
+fn intersections(ray: Ray, scene: &Scene) -> Option<&Intersect>{
   panic!()
 }
 
-fn traceRay(ray: Ray, scene: Scene, depth: u32) -> Color {
-    match intersections(ray, scene) {
+fn trace_ray(ray: Ray, scene: Scene, depth: u32) -> Color {
+    match intersections(ray, &scene) {
         Some(isect) => Color::background(),
         None => Color::background()
     }
 }
 
-fn defaultScene() -> Scene {
+fn default_scene() -> Scene {
     Scene {
-        things: vec![],
+        things: vec![Box::new(Sphere { center: Vector { x: 0.0, y: 1.0, z: -0.25}, radius: 1.0 })],
         lights: vec![],
         camera: Camera { pos: Vector {x: 3.0, y: 2.0, z: 4.0}, lookAt: Vector { x: -1.0, y: 0.5, z: 0.0}}
     }
