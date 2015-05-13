291c13704bfbfd2cca6302f91a65c0821b87cf4f
Luke Hoban <lukehoban@gmail.com>
2015-05-13 10:01:47 -0700

    Shade and framing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 extern crate image;
 
+use std::fmt::{Display, Formatter, Result};
 use std::path::Path;
 use std::f64::INFINITY;
 use image::ImageBuffer;
 
+#[derive(Copy, Clone)]
 struct Vector { x: f64, y: f64, z: f64 }
 impl Vector {
     fn times(k: f64, v: &Vector) -> Vector {
         Vector {x: k*v.x, y: k*v.y, z: k*v.z}
+    }
+    fn plus(v1: &Vector, v2: &Vector) -> Vector {
+        Vector {x: v1.x+v2.x, y: v1.y+v2.y, z: v1.z+v2.z}
     }
     fn minus(v1: &Vector, v2: &Vector) -> Vector {
         Vector {x: v1.x-v2.x, y: v1.y-v2.y, z: v1.z-v2.z}
@@ -19,7 +24,19 @@ impl Vector {
         (v.x*v.x + v.y*v.y + v.z*v.z).sqrt()
     }
     fn norm(v: &Vector) -> Vector {
-        panic!()
+        let mag = Vector::mag(v);
+        let div = if mag == 0.0 { INFINITY } else { 1.0 / mag };
+        Vector::times(div, &v)
+    }
+    fn cross(v1: &Vector, v2: &Vector) -> Vector {
+        Vector { x: v1.y*v2.z-v1.z*v2.y,
+                 y: v1.z*v2.x-v1.x*v2.z,
+                 z: v1.x*v2.y-v1.y*v2.x }
+    }
+}
+impl Display for Vector {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "<{},{},{}>", self.x, self.y, self.z)
     }
 }
 
@@ -28,12 +45,17 @@ impl Color {
   fn scale(k: f64, c: Color) -> Color {
       Color {r: k*c.r, g: k*c.g, b: k*c.b}
   }
-  fn to_drawing_color(c: Color) -> Color {
-    c
+  fn plus(v1: &Color, v2: &Color) -> Color {
+      Color {r: v1.r+v2.r, g: v1.g+v2.g, b: v1.b+v2.b}
   }
-  fn background() -> Color {
-      Color {r: 0.0, g: 0.0, b: 0.0 }
+  fn to_drawing_color(&self) -> [u8; 3] {
+    let legalize = |d| if d > 1.0 { 1.0 } else { d };
+    [ (legalize(self.r) * 255.0) as u8, (legalize(self.g) * 255.0) as u8, (legalize(self.b) * 255.0) as u8 ]
   }
+  fn white() -> Color { Color {r: 1.0, g: 1.0, b: 1.0 } }
+  fn grey() -> Color { Color {r: 0.5, g: 0.5, b: 0.5 } }
+  fn black() -> Color { Color {r: 0.0, g: 0.0, b: 0.0 } }
+  fn background() -> Color { Color::black() }
 }
 
 struct Ray { dir: Vector, start: Vector }
@@ -43,7 +65,16 @@ trait Thing {
     fn intersect<'a>(&'a self, ray: &Ray) -> Option<Intersect<'a>>;
 }
 struct Light { pos: Vector, color: Color }
-struct Camera { pos: Vector, lookAt: Vector }
+struct Camera { pos: Vector, forward: Vector, right: Vector, up: Vector }
+impl Camera {
+    fn new(pos: Vector, look_at: Vector) -> Camera {
+        let forward = Vector::norm(&Vector::minus(&look_at,&pos));
+        let down = Vector { x: 0.0, y: -1.0, z: 0.0};
+        let right = Vector::times(1.5, &Vector::norm(&Vector::cross(&forward, &down)));
+        let up = Vector::times(1.5, &Vector::norm(&Vector::cross(&forward, &right)));
+        Camera { pos: pos, forward: forward, right: right, up: up }
+    }
+}
 struct Scene { things: Vec<Box<Thing>>, lights: Vec<Light>, camera: Camera }
 
 struct Sphere { center: Vector, radius: f64 }
@@ -69,7 +100,6 @@ impl Thing for Sphere {
     }
 }
 
-
 fn intersections<'a>(ray: &Ray, scene: &'a Scene) -> Option<Intersect<'a>> {
   let mut closest = INFINITY;
   let mut closest_inter = None;
@@ -87,11 +117,38 @@ fn intersections<'a>(ray: &Ray, scene: &'a Scene) -> Option<Intersect<'a>> {
   closest_inter
 }
 
-fn trace_ray(ray: Ray, scene: Scene, depth: u32) -> Color {
+fn trace_ray(ray: &Ray, scene: &Scene, depth: u32) -> Color {
     match intersections(&ray, &scene) {
-        Some(isect) => Color::background(),
+        Some(isect) => {
+            println!("Intersection {}", isect.dist);
+            shade(&isect, &scene, &ray, depth)
+        },
         None => Color::background()
     }
+}
+
+const MAXDEPTH: u32 = 5;
+
+fn shade(isect: &Intersect, scene: &Scene, ray: &Ray, depth: u32) -> Color {
+    let d = ray.dir;
+    let pos = Vector::plus(&Vector::times(isect.dist, &d), &ray.start);
+    let normal = isect.thing.normal(pos);
+    let reflect_dir = Vector::minus(&d, &Vector::times(2.0, &Vector::times(Vector::dot(&normal, &d), &normal)));
+    let natural_color = Color::plus(
+        &Color::background(),
+        &get_natural_color(isect.thing, &pos, &normal, &reflect_dir, &scene));
+    let reflected_color = if depth >= MAXDEPTH { Color::grey() } else {
+        get_reflection_color(isect.thing, &pos, &normal, &reflect_dir, &scene, depth)
+    };
+    Color::plus(&natural_color, &reflected_color)
+}
+
+fn get_natural_color(isect: &Thing, pos: &Vector, normal: &Vector, reflectDir: &Vector, scene: &Scene) -> Color {
+    panic!();
+}
+
+fn get_reflection_color(isect: &Thing, pos: &Vector, normal: &Vector, reflectDir: &Vector, scene: &Scene, depth: u32) -> Color {
+    panic!();
 }
 
 fn default_scene() -> Scene {
@@ -99,7 +156,7 @@ fn default_scene() -> Scene {
         things: vec![Box::new(Sphere { center: Vector { x: 0.0, y: 1.0, z: -0.25}, radius: 1.0 }),
                      Box::new(Sphere { center: Vector { x: -1.0, y: 0.5, z: 1.5}, radius: 0.5 })],
         lights: vec![],
-        camera: Camera { pos: Vector {x: 3.0, y: 2.0, z: 4.0}, lookAt: Vector { x: -1.0, y: 0.5, z: 0.0}}
+        camera: Camera::new(Vector {x: 3.0, y: 2.0, z: 4.0}, Vector { x: -1.0, y: 0.5, z: 0.0})
     }
 }
 
@@ -112,13 +169,29 @@ fn main() {
 
     println!("Hello world!");
 
+    let width = 10;
+    let height = 10;
+    let scene = default_scene();
+    let ref camera = scene.camera;
+    let get_point = |x,y| {
+        let recenter_x = |x: f64| (x - ((width as f64) / 2.0))  / (2.0 * (width as f64));
+        let recenter_y = |y: f64| -(y - ((height as f64) / 2.0)) / (2.0 * (height as f64));
+        Vector::norm(
+            &Vector::plus(
+                &camera.forward,
+                &Vector::plus(
+                    &Vector::times(recenter_x(x as f64), &camera.right),
+                    &Vector::times(recenter_y(y as f64), &camera.up))))
+    };
+
     //Construct a new by repeated calls to the supplied closure.
-    let img = ImageBuffer::from_fn(512, 512, |x, y| {
-        if x % 2 == 0 {
-            image::Luma([0u8])
-        } else {
-            image::Luma([255u8])
-        }
+    let img = ImageBuffer::from_fn(width, height, |x, y| {
+        println!("Rendering {}, {}", x, y);
+        let ray = Ray { start: scene.camera.pos, dir: get_point(x,y) };
+        println!("Casting ray {}", ray.dir);
+        let color = trace_ray(&ray, &scene, 0).to_drawing_color();
+        println!("Color is |{},{},{}|", color[0], color[1], color[2]);
+        image::Rgb(color)
     });
 
     //Write the contents of this image to the Writer in PNG format.
